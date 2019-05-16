999406fc73504d1d66c821b1ac71257f634eaf49
Dion <dionagoes@gmail.com>
2019-05-16 21:02:34 +0700

    fix the card text layout

--- app/src/components/Card.vue
+++ app/src/components/Card.vue
@@ -1,17 +1,20 @@
 <template>
   <div style="max-width:35vh;" class="border-0 mx-2 my-2 card bg-dark text-white">
-  <img src="card2.jpg" class="card-img">
-  <div class="card-img-overlay">
-      <img style="height:18vh;" class="mt-4 ml-4" :src="monsters.url">
-    <h5 style="font-family: 'Pixel Digivolve'; font-size:1.7rem;" class="mt-3 ml-4 card-title">{{monsters.name}}</h5>
+    <img src="card2.jpg" class="card-img">
+    <div class="card-img-overlay" style="text-align: center">
+      <img style="height:18vh;" class="mt-4" :src="monsters.url">
+      <h5
+        style="font-family: 'Pixel Digivolve'; font-size:1.7rem;"
+        class="card-title mt-5"
+      >{{monsters.name}}</h5>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
-    props : ['monsters'],
-    name : 'Card'
+  props: ["monsters"],
+  name: "Card"
 };
 </script>
 
